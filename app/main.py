425ed76aa092f9e6bb26356167f5507b684588a8
THARUNESHWAR-369 <s.tharuneshwar.369@gmail.com>
2024-07-28 17:19:12 +0530

    docker file added

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
-app  : FastAPI = FastAPI()
+app  : FastAPI = FastAPI(title="Convin Backend Intern Project (Daily Expense Sharing App)")
 
 app.include_router(api_router, prefix="/api/v1")
